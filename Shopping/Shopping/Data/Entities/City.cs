e22e3172d10f56e9f2d0cc66fb6694e9ceb2027c
Mxdxstx <modestoivan19@gmail.com>
2023-03-25 16:41:26 -0600

    Parte 009 Min 34:20

--- Shopping/Shopping/Data/Entities/City.cs
+++ Shopping/Shopping/Data/Entities/City.cs
@@ -14,5 +14,8 @@ namespace Shopping.Data.Entities
 
         public State State { get; set; }
 
+        public ICollection<User> Users { get; set; }
+        
+
     }
 }
--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -10,7 +10,24 @@ builder.Services.AddDbContext<DataContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddTransient<SeedDb>();
+
+
 var app = builder.Build();
+SeedData();
+
+void SeedData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service.SeedAsync().Wait();
+    }
+
+}
 
 if (!app.Environment.IsDevelopment())
 {
